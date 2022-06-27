35e3cea9a4d3b98f74865c374b436cef30ee2375
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-06-27 21:44:14 +0700

    api login

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use Exception;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Rules\Password;
 
@@ -48,9 +49,39 @@ class UserController extends Controller
     public function login(Request $request)
     {
         try {
-            //code...
-        } catch (\Throwable $th) {
-            //throw $th;
+            $request->validate([
+                'username' => ['required'],
+                'password' => ['required']
+            ]);
+
+            // credential
+            $credential = request(['username', 'password']);
+
+            if(!Auth::attempt($credential))
+            {
+                return ResponseFormatter::error([$credential], 'Unauthorized', 401);
+            }
+
+            // get user
+            $user = User::where('username', $request->username)->first();
+            // cek password
+            if(!Hash::check($request->password, $user->password, []))
+            {
+                throw new Exception('Invalid credential');
+            }
+            
+            // token
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'token' => $token,
+                'user' => $user
+            ], 'Login successfully');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication failed', 500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
